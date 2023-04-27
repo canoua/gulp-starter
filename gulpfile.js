3b2f42a69d1d2c5f2b70894a31de03257c0c111f
akaryaka <a.karyaka.2dit@gmail.com>
2023-04-27 21:12:06 +0600

    add task fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 //не требуют установки
 const { src, dest, parallel, series, watch } = require('gulp');
 //***
+//html
 const htmlmin = require('gulp-htmlmin');
+//стили
 const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso');
+//скрипты
 const uglify = require('gulp-uglify-es').default;
+//конкатенация
 const concat = require('gulp-concat');
+//автопрефиксер
 const autoPrefixer = require('gulp-autoprefixer');
+//очистка папки с билдом
 const clean = require('gulp-clean');
+//конвертация шрифтов
+const fonter = require('gulp-fonter-unx');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
 function html() {
   return src('src/**.html')
@@ -48,6 +57,26 @@ function images() {
     .pipe(dest('build/images'))
 }
 
+function fonts() {
+  return src('src/static/fonts/*')
+    .pipe(fonter({
+      formats: ['woff', 'ttf', 'eot']
+    }))
+    .pipe(dest('build/fonts'))
+}
+
+function fonts2() {
+  return src('src/static/fonts/*')
+    .pipe(ttf2woff2())
+    .pipe(dest('build/fonts'))
+}
+
+
+//перед работой сконвертировать шрифты
+exports.fonts = fonts
+exports.fonts2 = fonts2
+//для тестов
 exports.images = images
 exports.cleanBuild = cleanBuild
+//build
 exports.build = series(cleanBuild, parallel(html,images, styles, scripts))
