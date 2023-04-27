b2c180d0dcc1f02b3b6f6ca4bdd2d7f755b4c99b
akaryaka <a.karyaka.2dit@gmail.com>
2023-04-27 18:41:30 +0600

    add task html

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 //не требует установки
-const { src, dest, parallel, series } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
 const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const autoPrefixer = require('gulp-autoprefixer');
-const csso = require('gulp-csso');
 const clean = require('gulp-clean');
+
+function html() {
+  return src('src/**.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(dest('build'))
+}
 
 function scripts() {
   return src('src/scripts/*.js')
@@ -30,5 +39,6 @@ function cleanBuild() {
 
 // exports.js = js
 // exports.cleanBuild = cleanBuild
+exports.html = html
 exports.styles = styles
 exports.build = series(cleanBuild, parallel(styles, scripts))
