35aa5e14511e633775c0b3e9ee25c143c6c0d206
akaryaka <a.karyaka.2dit@gmail.com>
2023-04-27 18:33:44 +0600

    add task cleanBuild

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const autoPrefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const clean = require('gulp-clean');
 
 function scripts() {
   return src('src/scripts/*.js')
@@ -17,9 +19,16 @@ function styles() {
     .pipe(concat('style.gulpcss'))
     .pipe(autoPrefixer(['last 2 versions']))
     .pipe(sass().on('error', sass.logError))
+    .pipe(csso())
     .pipe(dest('build/styles/'))
 }
 
+function cleanBuild() {
+  return src('build')
+    .pipe(clean())
+}
+
 // exports.js = js
+// exports.cleanBuild = cleanBuild
 exports.styles = styles
-exports.build = parallel(styles, scripts);
+exports.build = series(cleanBuild, parallel(styles, scripts))
